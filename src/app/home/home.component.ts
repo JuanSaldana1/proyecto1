11c19423831988bd545ed635f696ad7fd10fc95d
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-03-03 21:45:42 +0100

    Funciona el select

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {MdbModalRef, MdbModalService} from "mdb-angular-ui-kit/modal";
+import {Modal1Component} from "./modal1/modal1.component";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  modalRef: MdbModalRef<HomeComponent> | null = null;
+
+  constructor(
+    private modalService: MdbModalService
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  openModal() {
+    this.modalRef = this.modalService.open(Modal1Component);
+  }
+
+
+}
