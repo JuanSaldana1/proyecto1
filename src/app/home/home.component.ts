e5ee3ffe53befbe8ddf7a11da3a032abb22a1d40
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-07 13:51:32 +0200

    modal fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,14 @@
+/* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
 import { Component, OnInit } from '@angular/core';
-import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
-import { Modal1Component } from './modal1/modal1.component';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers: [MdbModalService],
+  providers: [],
 })
 export class HomeComponent implements OnInit {
-  modalRef: MdbModalRef<HomeComponent> | null = null;
-
-  constructor(private modalService: MdbModalService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  openModal() {
-    this.modalRef = this.modalService.open(Modal1Component);
-  }
 }
